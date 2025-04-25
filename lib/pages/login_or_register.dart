4f3604cb385143421929c7eccfed20457a97842a
Akshat089 <82509785+Akshat089@users.noreply.github.com>
2025-04-25 22:39:37 +0530

    Update login_or_register.dart

--- lib/pages/login_or_register.dart
+++ lib/pages/login_or_register.dart
@@ -5,55 +5,53 @@ import 'package:flutter/material.dart';
 
 class LoginOrRegister extends StatelessWidget {
   const LoginOrRegister({super.key});
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    final Size screenSize = MediaQuery.of(context).size;
+    final double screenWidth = screenSize.width;
+    final double screenHeight = screenSize.height;
+
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 100,
+        toolbarHeight: screenHeight * 0.12,
         backgroundColor: const Color.fromARGB(255, 212, 115, 229),
       ),
       body: Center(
         child: Container(
+          width: screenWidth * 1.0,
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
           color: const Color.fromARGB(255, 225, 194, 220),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              SizedBox(height: screenHeight * 0.1),
               SizedBox(
-                height: 150,
-                width: 50,
-              ),
-              SizedBox(
-                width: 311,
-                height: 226,
+                width: screenWidth * 0.7,
+                height: screenHeight * 0.3,
                 child: Image.asset(
                   'lib/images/logo.jpg',
                   fit: BoxFit.contain,
                 ),
               ),
-              SizedBox(
-                height: 50,
-              ),
+              SizedBox(height: screenHeight * 0.05),
               Center(
                 child: Text(
                   "Welcome",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: screenWidth * 0.06),
                 ),
               ),
-              SizedBox(
-                height: 20,
-              ),
+              SizedBox(height: screenHeight * 0.02),
               Center(
                 child: Text(
                   "Login or register to get answers to all your finance related questions",
-                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: screenWidth * 0.045),
                 ),
               ),
+              SizedBox(height: screenHeight * 0.05),
               SizedBox(
-                height: 50,
-              ),
-              SizedBox(
-                height: 128,
-                width: 263,
+                width: screenWidth * 0.8,
                 child: Column(
                   children: [
                     Buttons(
@@ -66,18 +64,18 @@ class LoginOrRegister extends StatelessWidget {
                       text: Text(
                         "Login",
                         style: TextStyle(
-                          fontSize: 18,
+                          fontSize: screenWidth * 0.05,
                           fontWeight: FontWeight.bold,
                           color: Colors.white,
-                          decoration: TextDecoration.none, // Remove underline
+                          decoration: TextDecoration.none,
                         ),
                       ),
                       color: const Color.fromARGB(255, 222, 140, 220),
                       color1: const Color.fromARGB(255, 214, 122, 228),
-                      height: 55,
-                      width: 263,
+                      height: screenHeight * 0.07,
+                      width: screenWidth * 0.8,
                     ),
-                    SizedBox(height: 18),
+                    SizedBox(height: screenHeight * 0.025),
                     Buttons(
                       onTap: () {
                         Navigator.push(
@@ -88,16 +86,16 @@ class LoginOrRegister extends StatelessWidget {
                       text: Text(
                         "Sign Up",
                         style: TextStyle(
-                          fontSize: 18,
+                          fontSize: screenWidth * 0.05,
                           fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 222, 140, 220),
-                          decoration: TextDecoration.none, // Remove underline
+                          color: const Color.fromARGB(255, 222, 140, 220),
+                          decoration: TextDecoration.none,
                         ),
                       ),
                       color: Colors.white,
                       color1: const Color.fromARGB(255, 214, 122, 228),
-                      height: 55,
-                      width: 263,
+                      height: screenHeight * 0.07,
+                      width: screenWidth * 0.8,
                     ),
                   ],
                 ),
